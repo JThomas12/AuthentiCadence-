14295acf68b28a6432b6643e222531cad780856f
dannhxwan6 <103629433+dannhxwan6@users.noreply.github.com>
2022-09-24 13:46:31 -0500

    user profile finished

--- main.py
+++ main.py
@@ -1,29 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import training
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print(training.train(1))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-def train(train_data):
-    """
-    INPUT
-        train_data; n-length list of k-length lists,
-            where the ith element of the jth list is the ith keystroke measurement of the jth password entry
-    OUTPUT
-        centroid; the Euclidean centroid of the train_data
-        mean; mean of the log of the Euclidean norms between training data and centroid
-        std; standard deviation of the log of the Euclidean norms between training data and centroid
-    """
 
---
+++ training.py
@@ -0,0 +1,80 @@
+# yo no way we got the whole squad here 🎉
+
+import numpy as np
+
+class UserProfile:
+    def __init__(self, trainData):
+        """
+        INPUT
+            trainData; numpy array, where the ij element is the jth keystroke element of the ith password entry
+        """
+        self.trainData = trainData
+        self.centroid, self.distMean, self.distStd = self.train(trainData)
+        
+    def __str__(self):
+        return str(np.shape(self.trainData))
+
+    def getTrainData(self):
+        return self.trainData
+    
+    def getCentroid(self):
+        return self.centroid
+
+    def getDistMean(self):
+        return self.distMean
+
+    def getDistStd(self):
+        return self.distStd
+
+    def train(self, trainData):
+        """
+        INPUT
+            trainData; numpy array, where the ij element is the jth keystroke element of the ith password entry
+        OUTPUT
+            centroid; the Euclidean centroid of the train_data
+            mean; mean of the log of the Euclidean norms between training data and centroid
+            std; standard deviation of the log of the Euclidean norms between training data and centroid
+        """
+        centroid = np.average(trainData, axis=0)
+        dist = np.log(np.linalg.norm(trainData - centroid, axis=1))        
+        return centroid, np.average(dist), np.std(dist)
+
+
+    def verifyCadence(self, keystroke, sensitivity):
+        """
+        INPUT
+            keystroke; numpy array of new keystroke entry
+            sensitivity; number of standard deviations to account for in verification threshold
+        OUTPUT
+            True/False to indicate validity of keystroke
+        """
+        if(keystroke.size  != self.trainData.shape[1]):
+            return False
+        error = np.log(np.linalg.norm(keystroke - self.centroid))
+        threshold = self.distMean + sensitivity * self.distStd
+        if (error < threshold):
+            self.updateData(keystroke)
+            return True
+        return False
+
+    def updateData(self, keystroke):
+        """
+        Updates the training data to include new valid detected keystroke entry. Then retrains model.
+        INPUT
+            keystroke; numpy array of new keystroke entry
+        """
+        self.trainData = np.vstack([self.trainData, keystroke])
+        self.centroid, self.distMean, self.distStd = self.train(self.trainData)
+
+dat = np.array([[3, 2, 4.5], [2.5, 1, 5.5], [3.5, 3, 4], [3, 2, 6]])
+profile = UserProfile(dat)
+sensitivity = 1.5
+print(profile.verifyCadence(np.array([1, 2, 3]), sensitivity))
+print(profile.verifyCadence(np.array([3, 2, 5.1]), sensitivity))
+print(profile.verifyCadence(np.array([3, 2, 4]), sensitivity))
+print(profile.verifyCadence(np.array([2, 3, 7]), sensitivity))
+print(profile.verifyCadence(np.array([2, 3, 10]), sensitivity))
+print(profile.verifyCadence(np.array([3, 2, 5, 0]), sensitivity))
+
+print(profile.getTrainData())
+print(profile)
