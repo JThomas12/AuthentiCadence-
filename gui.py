2d499061981da7a570add7489df90df242f5ad59
Caleb McKinney <cam26@rice.edu>
2022-09-24 23:26:01 -0500

    Finished new frontend

--- gui.py
+++ gui.py
@@ -1,13 +1,34 @@
+from sre_parse import State
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
+from tkinter.font import BOLD
+import customtkinter
 import time
 
 from user_profile import UserProfile
 
 class TestAuthentiCadence:
     def __init__(self):
-        self.window = tk.Tk()
+        customtkinter.set_appearance_mode("Dark")
+        customtkinter.set_default_color_theme("blue")
+
+        self.window = customtkinter.CTk()
+        self.window.title("AuthentiCadence")
+        self.window.geometry("800x500")
+
         self.user = UserProfile()
+
+        customtkinter.CTkLabel(text="AuthentiCadence", text_font=("Calibri",25)).pack(pady=10)
+        
+        customtkinter.CTkLabel(text="Options:", text_font=("Calibri Bold", 16)).pack(pady=10)
+        customtkinter.CTkButton(self.window, text="Refresh Window", command=self.refresh).pack()
+
+        self.show_viz_check = tk.IntVar(value=1)
+        customtkinter.CTkCheckBox(self.window, text="Show Authentication Visualization", variable=self.show_viz_check, onvalue=1, offvalue=0).pack()
+        
+        self.hide_password_check = tk.IntVar()
+        customtkinter.CTkCheckBox(self.window, text="Hide Password Entry", variable=self.hide_password_check, onvalue=1, offvalue=0, command=self.updateShowPass).pack()
 
         self.num_trials = 0
 
@@ -17,42 +38,43 @@ class TestAuthentiCadence:
         self.prev_test_change_time = None
 
         # Create password and train model =================================================
+        customtkinter.CTkLabel(text="Password Training:", text_font=("Calibri Bold", 16)).pack(pady=(10, 0))
 
-        #show_hide_pass_box = tk.Checkbutton(text="", )
+        train_pass_label = customtkinter.CTkLabel(self.window, text = "Create a password below with your favorite rhythm!")
+        train_pass_label.pack()
 
-        train_pass_label = tk.Label(self.window, text = "Create a password below with your favorite rhythm!")
-        train_pass_label.pack(padx=50, pady=5)
-
-        self.num_trials_label = tk.Label(self.window, text = "0 Trials")
+        self.num_trials_label = customtkinter.CTkLabel(self.window, text = "0 Trials")
         self.num_trials_label.pack()
 
         self.train_pass_entry = tk.StringVar()
         self.train_pass_entry.trace("w", lambda name, index, mode, sv=self.train_pass_entry: self.onCreatePassChange(sv))
 
-        train_pass_box = tk.Entry(self.window, textvariable=self.train_pass_entry)
-        train_pass_box.pack(padx=50, pady=5)
+        self.train_pass_box = customtkinter.CTkEntry(self.window, textvariable=self.train_pass_entry)
+        self.train_pass_box.pack()
 
-        train_pass_button = tk.Button(self.window, text="Create/Train Password", command=self.trainPass)
-        train_pass_button.pack()
+        train_pass_button = customtkinter.CTkButton(self.window, text="Create/Train Password", command=self.trainPass)
+        train_pass_button.pack(pady=(5, 0))
 
         # Test Model ====================================================
+        customtkinter.CTkLabel(text="Password Testing:", text_font=("Calibri Bold", 16)).pack(pady=(10, 0))
 
         self.cur_test_keystrokes = []
         self.prev_test_str = ""
 
-        test_pass_label = tk.Label(self.window, text = "Test your trained password below:")
-        test_pass_label.pack(padx=50, pady=5)
+        test_pass_label = customtkinter.CTkLabel(self.window, text = "Test your trained password below:")
+        test_pass_label.pack()
 
         self.test_pass_entry = tk.StringVar()
         self.test_pass_entry.trace("w", lambda name, index, mode, sv = self.test_pass_entry: self.onTestPassChange(sv))
         
-        self.test_pass_box = tk.Entry(self.window, textvariable = self.test_pass_entry, state='disabled')
-        self.test_pass_box.pack(padx=50, pady=5)
+        self.test_pass_box = customtkinter.CTkEntry(self.window, textvariable = self.test_pass_entry, state=tk.DISABLED)
+        self.test_pass_box.pack()
         
-        self.test_pass_button = tk.Button(self.window, text="Enter Password", command=self.testPass, state='disabled')
-        self.test_pass_button.pack()
+        self.test_pass_button = customtkinter.CTkButton(self.window, text="Enter Password", command=self.testPass, state=tk.DISABLED)
+        self.test_pass_button.pack(pady=(5, 0))
 
         self.window.mainloop()
+        self.window.update()
 
     def onCreatePassChange(self, sv):
         cur_str = sv.get()
@@ -77,6 +99,7 @@ class TestAuthentiCadence:
             # Validate password is correct
             if hash(self.train_pass_entry.get()) == self.user.password:
                 # Add current time data to user's time dataset
+                self.num_trials += 1
                 self.user.cadence_profile.updateData(self.cur_train_keystrokes)
                 print("Correct password added to user!")
             else:
@@ -88,16 +111,16 @@ class TestAuthentiCadence:
             # Add current time data to user's time datset
             self.user.cadence_profile.updateData(self.cur_train_keystrokes)
             print("New password added to user!")
+            self.num_trials += 1
 
         # Reset entry box
         self.train_pass_entry.set("")
-        self.num_trials += 1
-        self.num_trials_label.config(text = str(self.num_trials) + " Trials")
+        self.num_trials_label.configure(text = str(self.num_trials) + " Trials")
         
         # Enable testing functionality if at least two trials
         if self.num_trials >= 2:
-            self.test_pass_box.config(state="normal")
-            self.test_pass_button.config(state="normal")
+            self.test_pass_box.configure(state="normal")
+            self.test_pass_button.configure(state="normal")
 
         print(self.user.cadence_profile.getTrainData())
 
@@ -120,14 +143,31 @@ class TestAuthentiCadence:
     def testPass(self):
         if hash(self.test_pass_entry.get()) != self.user.password:
             # If password incorrect, do nothing
-            messagebox.showerror("Incorrect password!", "Please enter the correct password.")
+            messagebox.showerror("Incorrect password (value)!", "Please enter the password value.")
         else:
             isVerified = self.user.verifyPassword(self.test_pass_entry.get(), self.cur_test_keystrokes)
-            if isVerified:
-                print("User successfully logged in")
+            showViz = self.show_viz_check.get()
+            
+            if showViz:
+                self.user.cadence_profile.visualizeCadence(self.cur_test_keystrokes, isVerified)
             else:
-                print("User failed to authenticate")
-            self.user.cadence_profile.visualizeCadence(self.cur_test_keystrokes, isVerified)
+                if isVerified:
+                    messagebox.showinfo("Correct password!", "User sucessfuly authenticated.")
+                else:
+                    messagebox.showerror("Incorrect password (rhythm)!", "Please enter the correct password rhythm.")
+
         self.test_pass_entry.set("")
+
+    def updateShowPass(self):
+        if self.hide_password_check.get():
+            self.test_pass_box.configure(show="*")
+            self.train_pass_box.configure(show="*")
+        else:
+            self.test_pass_box.configure(show="")
+            self.train_pass_box.configure(show="")
+    
+    def refresh(self):
+        self.window.destroy()
+        TestAuthentiCadence()
 
 TestAuthentiCadence()
