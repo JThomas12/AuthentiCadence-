d56d2398f346110e3a15d769d0ea36c82d90d41f
SeventhPrize <gml8@rice.edu>
2022-09-24 20:37:29 -0500

    docstrings added

--- cadence_profile.py
+++ cadence_profile.py
@@ -5,26 +5,20 @@ import matplotlib.pyplot as plt
 
 class CadenceProfile:
     def __init__(self, sensitivity=1):
-        """
+        '''
         INPUT
-            trainData; numpy array, where the ij element is the jth keystroke element of the ith password entry
             sensitivity; threshold number of standard deviations away from expected cadence where invalid cadences are cut off
-        """
-        self.trainData = np.array([])
-        self.centroid = np.array([])
+        '''
+        self.trainData = np.array([]) #numpy array, where the ij element is the jth keystroke element of the ith password entry
+        self.centroid = np.array([]) #numpy array, where the jth element is the average of the jth components of trainData
         self.distMean = np.array([])
         self.distStd = 0
         self.sensitivity = sensitivity
-
-        # trainData = np.array([self.timeToRatio(keystroke) for keystroke in timeData])
-        # self.trainData = trainData
-        # self.centroid, self.distMean, self.distStd = self.train(trainData)
-        # self.sensitivity = sensitivity
-
-    def timeToRatio(self, timeKeystroke):
-        return np.array(timeKeystroke) / sum(timeKeystroke)
         
     def __str__(self):
+        '''
+        Printing this class gives the size of the training data and the number of keystrokes
+        '''
         return str(np.shape(self.trainData))
 
     def getTrainData(self):
@@ -39,15 +33,24 @@ class CadenceProfile:
     def getDistStd(self):
         return self.distStd
 
+    def timeToRatio(self, timeKeystroke):
+        '''
+        Converts a list of keystroke durations into a list of keystroke durations normalized to the sum fo their durations
+        INPUT
+            timeKeystroke; a list keystroke durations
+        '''
+        return np.array(timeKeystroke) / sum(timeKeystroke)
+
     def train(self, trainData):
-        """
+        '''
+        Trains the binary classifier to recognize valid cadence.
         INPUT
             trainData; numpy array, where the ij element is the jth keystroke element of the ith password entry
         OUTPUT
             centroid; the Euclidean centroid of the train_data
             mean; mean of the log of the Euclidean norms between training data and centroid
             std; standard deviation of the log of the Euclidean norms between training data and centroid
-        """
+        '''
         centroid = np.average(trainData, axis=0)
         if(len(trainData.shape) == 1):
             dist = np.log(np.linalg.norm(trainData - centroid)) 
@@ -56,51 +59,71 @@ class CadenceProfile:
         return centroid, np.average(dist), np.std(dist)
 
     def verifyCadence(self, timeKeystroke, visualize=False):
-        """
+        '''
         INPUT
-            keystroke; numpy array of new keystroke entry
-            sensitivity; number of standard deviations to account for in verification threshold
+            timeKeystroke; python list new keystroke entry (measures time elapsed between keystrokes, not necessarily with normalization)
+            visualize; boolean whether to visualize the match graphically
         OUTPUT
             True/False to indicate validity of keystroke
-        """
+        '''
         print("verifyCadence: ",timeKeystroke )
         keystroke = self.timeToRatio(timeKeystroke)
+        
+        # if the keystroke vector length is not consistent with the established length, reject
         if(keystroke.size  != self.trainData.shape[1]):
             return False
+
+        # compute distance between each vector and the centroid
         error = math.log(np.linalg.norm(keystroke - self.centroid))
         threshold = self.distMean + self.sensitivity * self.distStd
+
+        # check if vector is within threshold
         if (error < threshold):
             self.updateData(timeKeystroke)
             flag = True
         else:
             flag = False
+        
+        # visualize
         if (visualize):
             self.visualizeCadence(keystroke, flag)
         return (flag)
 
     def updateData(self, timeKeystroke):
-        """
+        '''
         Updates the training data to include new valid detected keystroke entry. Then retrains model.
         INPUT
-            keystroke; numpy array of new keystroke entry
-        """
+            timeKeystroke; python list new keystroke entry (measures time elapsed between keystrokes, not necessarily with normalization)
+        '''
         keystroke = self.timeToRatio(timeKeystroke)
         if(self.trainData.size == 0):
             self.trainData = keystroke
         else:
-            self.trainData = np.vstack([self.trainData, keystroke])
-        self.centroid, self.distMean, self.distStd = self.train(self.trainData)
+            self.trainData = np.vstack([self.trainData, keystroke]) # add vector to training corpus
+        self.centroid, self.distMean, self.distStd = self.train(self.trainData) # retrain
 
     def visualizeCadence(self, timeKeystroke=[], flag=False):
-        keystroke = self.timeToRatio(timeKeystroke)
+        '''
+        Plots the target cadence range against an input cadence
+        INPUT
+            timeKeystroke; python list new keystroke entry (measures time elapsed between keystrokes, not necessarily with normalization)
+            flag; boolean whether the input keystroke was admitted or not
+        '''
+        keystroke = self.timeToRatio(timeKeystroke) # normalize times of keystroke vector
         numTrain, numKey = self.trainData.shape
         keyInds = range(0, numKey)
+        
+        # compute target cadence bounds
         errRange = 1 / math.sqrt(numTrain) * math.exp(self.distMean + self.sensitivity * self.distStd)
         loBound = self.centroid - errRange
         hiBound = self.centroid + errRange
+
+        # plot cadence bounds
         plt.fill_between(keyInds, loBound, hiBound,
                             interpolate=True, color="green", alpha=0.2,
                             label="Target cadence")
+        
+        # plot tested keystroke
         if (keystroke.any()):
             if (flag):
                 plt.plot(keyInds, keystroke,
